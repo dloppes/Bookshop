ba241de17e94c22d56a33307206c5ecc33b32b11
dloppes <d.loppes.soares@gmail.com>
2020-10-01 11:42:22 +0100

    Entities changes
    Attributes added to the entities

--- src/Book.java
+++ src/Book.java
@@ -1,8 +1,46 @@
 
+/**
+ * 
+ * @author Daniel Lopes
+ *
+ *         Book entity and all of its particular methods
+ */
+
 public class Book {
 
-	public Book() {
-		// TODO Auto-generated constructor stub
+	protected int id;
+	protected String title;
+	protected String author;
+	protected String director;
+	protected int year;
+
+	public Book(String title, String author, String director, int year) {
+
+		/*
+		 * This constructor is used when I first create the book entity, It has no ID
+		 * because the ID might be generated automatically when inserted into the
+		 * database.
+		 */
+
+		this.title = title;
+		this.author = author;
+		this.director = director;
+		this.year = year;
+	}
+
+	public Book(int id, String title, String author, String director, int year) {
+
+		/*
+		 * Once the book is inserted into the database I will have an ID, therefore I
+		 * can query and get the ID from the system in order to create an entity with
+		 * the ID.
+		 */
+
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.director = director;
+		this.year = year;
 	}
 
 }
--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,23 @@
 
-public class Employee {
+public class Employee extends Person {
 
-	public Employee() {
+	protected String username;
+	protected String password;
+
+	public Employee(String fName, String lName, String email, String username, String password) {
+		super(fName, lName, email);
+
+		this.username = username;
+		this.password = password;
+
+	}
+
+	public Employee(int id, String fName, String lName, String email, String username, String password) {
+		super(id, fName, lName, email);
 		// TODO Auto-generated constructor stub
+
+		this.username = username;
+		this.password = password;
 	}
 
 }
