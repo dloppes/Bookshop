993488dc3bc762f8027e9c428c1caa0f4ba25a29
dloppes <d.loppes.soares@gmail.com>
2020-12-04 19:58:25 +0000

    removing unused imports

--- src/Database.java
+++ src/Database.java
@@ -7,7 +7,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-import javax.swing.text.html.FormView;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
